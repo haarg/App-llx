afbff2abe2d2f45ae915acb4ff08c68fc8e64c15
Graham Knop <haarg@haarg.org>
2024-02-09 04:14:21 +0100

    initial version

---
+++ lib/App/llx.pm
@@ -0,0 +1,37 @@
+package App::llx;
+use strict;
+use warnings;
+
+our $VERSION = '1.000000';
+$VERSION =~ tr/_//d;
+
+1;
+__END__
+
+=head1 NAME
+
+llx - Execute in a local::lib
+
+=head1 DESCRIPTION
+
+See L<llx>.
+
+=head1 AUTHOR
+
+haarg - Graham Knop (cpan:HAARG) <haarg@haarg.org>
+
+=head1 CONTRIBUTORS
+
+None so far.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2024 the App::llx L</AUTHOR> and L</CONTRIBUTORS>
+as listed above.
+
+=head1 LICENSE
+
+This library is free software and may be distributed under the same terms
+as perl itself. See L<https://dev.perl.org/licenses/>.
+
+=cut
---
+++ t/basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use Test::More;
+
+use App::llx;
+
+ok 1;
+
+done_testing;
